37e3af8ae568d1d5f0847a204374f2cf57687773
Hans Kilian <hans@kilianconsult.dk>
2017-06-30 03:50:45 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace webapi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls("http://*:14114")
+                .Build();
+
+            host.Run();
+        }
+    }
+}
